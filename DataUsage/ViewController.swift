ba52ed0d1318dd56337f97e3d9b3358960948cfc
S.s.Sailesh Kumar <sai.suri@honeywell.com>
2019-10-20 14:22:39 +0530

    created data usage project

---
+++ DataUsage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataUsage
+//
+//  Created by Suri,Sai Sailesh Kumar on 20/10/19.
+//  Copyright © 2019 Sailesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
