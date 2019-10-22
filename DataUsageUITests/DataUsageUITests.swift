2b6f74a7d2651ce86f240d4d4249751872a7f543
S.s.Sailesh Kumar <sai.suri@honeywell.com>
2019-10-22 15:52:30 +0530

    added ui test cases and change workspace settings to legacy to resolve issue of running UITests

--- DataUsageUITests/DataUsageUITests.swift
+++ DataUsageUITests/DataUsageUITests.swift
@@ -9,6 +9,9 @@
 import XCTest
 
 class DataUsageUITests: XCTestCase {
+    
+    var app: XCUIApplication!
+
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,7 +20,8 @@ class DataUsageUITests: XCTestCase {
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app = XCUIApplication()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -30,5 +34,23 @@ class DataUsageUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    /// To Test weather the year data is displayed
+    func testYear2018DataTableDisplayed() {
+        let table = app.tables
+        XCTAssertTrue(table.staticTexts["2018"].exists, "validating data displayed over cell")
+        XCTAssert(table.cells.count != 0, "checking for the data displayed")
+    }
+    
+    func testTapOnDecreaseButtonOverTable() {
+        let table = app.tables
+        let cell = table.cells.element(boundBy: 3)
+        let button = cell.buttons["decrease"]
+        button.tap()
+        sleep(5)
+        XCTAssert(app.otherElements["popover"].exists, "checks if popover is presented")
+    }
+    
 
 }
+
